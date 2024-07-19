a695c20ab485ac836efdc2315db363691c34f4d8
Gabz047 <gabe1612mail@gmail.com>
2024-07-19 11:57:20 -0300

    Incio da adicao com passageid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 import API from '@/api/api';
 import { onMounted, ref } from 'vue';
 import { useAuthStore } from '@/stores/auth/auth';
+import '@passageidentity/passage-elements/passage-auth'
+
 const api = new API
 const users = ref([])
 const store = useAuthStore()
 
-onMounted(async()=>{
+onMounted(async () =>{
   if(store.islogged == true) {
     users.value = await api.Listar('api/usuarios/')
     console.log(users.value)
@@ -18,4 +20,7 @@ onMounted(async()=>{
   <main>
     <p v-for="user in users"> {{ user.username }} </p>
   </main>
+  <div class="authContainer">
+  <passage-auth :app-id="appId"></passage-auth>
+</div>
 </template>
